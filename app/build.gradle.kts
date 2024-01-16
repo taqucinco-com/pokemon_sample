932bceb0e854161dad717a7f3ce89667535e2ce7
taqucinco <someone.said.so@gmail.com>
2024-01-16 14:00:39 +0900

    バージョニング

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.taqucinco.pokemon_sample"
         minSdk = 30
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
